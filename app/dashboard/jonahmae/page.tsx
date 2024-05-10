36a052a8b1b05016d468bde44f808c1820871033
Ursus Acol <ursusacol16@gmail.com>
2024-05-10 10:40:14 +0800

    save all

--- app/dashboard/jonahmae/page.tsx
+++ app/dashboard/jonahmae/page.tsx
@@ -1,3 +1,17 @@
 export default function Page() {
-  return <p>Invoices Page</p>;
+  return (
+    <>
+      <p>Hello JonahMae haha</p>
+      <img
+        alt="Screenshots of the dashboard project showing mobile version"
+        loading="lazy"
+        width="560"
+        height="620"
+        decoding="async"
+        data-nimg="1"
+        src="/_next/image?url=%2Fjonah-mae.jpg&amp;w=1200&amp;q=75"
+      ></img>
+      <p>I wuv u</p>
+    </>
+  );
 }
